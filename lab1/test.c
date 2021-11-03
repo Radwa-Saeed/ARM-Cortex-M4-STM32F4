f9ef98354babc6975a21b18f28d55fc99dfad445
Radwa-Saeed <61630444+Radwa-Saeed@users.noreply.github.com>
2021-11-03 21:37:40 +0200

    00

---
+++ lab1/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "fun.h"
+int main(void)
+{
+    // ############ calculate v and s #############
+    // float u, a, t;
+    // printf("ENTER THE INITIAL VELOCITY...\n");
+    // scanf("%f", &u);
+    // printf("ENTER THE ACCELERATION ...\n");
+    // scanf("%f", &a);
+    // printf("ENTER THE DISTANCE...\n");
+    // scanf("%f", &t);
+    // finalVelocity(u, a, t);
+
+    // ########### calculate sum, avg, prod, max and min ##########
+    // int num1, num2, num3;
+    // printf("ENTER THREE DIFFERENT NUMBERS ... ");
+    // scanf("%d  %d  %d", &num1, &num2, &num3); // space between specifiers means you should seperate your input numbers by spaces
+    // calculat(num1, num2, num3);
+
+    // ########### calculate time from seconds
+    // int h, m, s, total;
+    // printf("PLEASE ENTER THE TOTAL TIME ELAPSED IN SECONDS ... ");
+    // scanf("%d", &total);
+    // h = total / 3600;
+    // m = (total - (h * 3600)) / 60;
+    // s = (total - (h * 3600) - (m * 60));
+    // printf("Time %d:%d:%d", h, m, s);
+
+    return 0;
+};
