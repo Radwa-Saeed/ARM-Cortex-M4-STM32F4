ca52aa129ef306641712bff7f0f05ecf52f1e837
Radwa-Saeed <61630444+Radwa-Saeed@users.noreply.github.com>
2021-12-27 18:06:03 +0200

    record

---
+++ task4/Core/Src/main.c
@@ -0,0 +1,41 @@
+/*
+ * Author : Radwa_Saeed
+ * Date   : 7/12/2021
+ * Version: 1.0.0
+ */
+#include "GPIO.h"
+
+void Toggle_Led_ISR(void);  // Called only in startup file
+void Led_Button_INIT(void);
+
+int main(void) {
+	EXTI_EnableClock(); 					// ENABLE EXTERNAL INTERRUPT CLOCK CONFIG
+	(*SYSCFG_EXTICR1) &=(0x00 << 0x00); 	//  PORT A as source input for the EXTI0 (bin 0)
+	(*EXTI_IMR) |=(0x01 << 0x00); 			// Interrupt request from line 0 is not masked
+	(*EXTI_FTSR) |=(0x01 << 0x00); 			//  Falling trigger selection register
+	(*NVIC_ISER0) = (0x01 << 0x06);
+
+	Led_Button_INIT();
+
+  while(1){
+  }
+  return 0;
+}
+void Toggle_Led_ISR(void){
+	(*EXTI_PR) |= (0x01 << 0x00); 			// Clear EXTI Flag
+	if (GPIO_ReadPin(0, 1) == 0){
+		GPIO_WritePin(0, 1, 1);
+	}
+	else if (GPIO_ReadPin(0, 1) == 1){
+		GPIO_WritePin(0, 1, 0);
+	}
+}
+
+void Led_Button_INIT(void){
+	// LED & PUSH BUTTON CONFIG
+	GPIO_EnableClock(0);
+	GPIO_Init(0, 0, INPUT, PULL_UP);
+	GPIO_Init(0, 1, OUTPUT, PUSH_PULL);
+}
+
+
